75ed68aa38fb678b234cf89c4bab9b05aad6a450
Your Name <you@example.com>
2023-03-28 15:34:57 +0700

    update-run-files

---
+++ test/test.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+from show_result import plot_bboxes
+
+model = YOLO("D:/TIPA/License/Read_License_YOLOv8/test/ORC.pt")
+image = Image.open("D:/TIPA/License/Read_License_YOLOv8/test/1.jpg")
+image_arr = np.asarray(image)
+results = model.predict(image_arr)
+print(results[0].boxes.boxes)
+#plot_bboxes(image_arr, results[0].boxes.boxes, score=False, conf=0.5)
