6c70471c37ffe9c3bcb439dfb775728e29c2240d
Your Name <you@example.com>
2023-03-30 13:56:47 +0700

    re-construct

--- test/test.py
+++ test/test.py
@@ -1,5 +1,4 @@
 from ultralytics import YOLO
-from PIL import Image
 import numpy as np
 import cv2
 from show_result import plot_bboxes
@@ -8,11 +7,14 @@ from pre_process import pre_processing
 detect_model = YOLO("D:/TIPA/License/Read_License_YOLOv8/models/Detect.pt")
 ORC_model = YOLO("D:/TIPA/License/Read_License_YOLOv8/models/ORC.pt")
 
-image_name = "6.jpg"
-image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/{image_name}")
+image_name = "IPSS2.7_s7aw.cn_2017-10-24.12-14-05.jpg"
+#image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/{image_name}")
+#image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/TEST BIEN SO/TEST BIEN SO/{image_name}")
+image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/TEST BIEN SO/TEST BIEN SO/no_support/{image_name}")
 image_arr = np.asarray(image)
 
 license_results = detect_model.predict(image_arr)
+
 
 boxes = license_results[0].boxes.xyxy.to('cpu').numpy().astype(int)
 confidences = license_results[0].boxes.conf.to('cpu').numpy().astype(float)
@@ -25,4 +27,5 @@ for box, conf, label in zip(boxes, confidences, labels):
 license_img = pre_processing(image_crop)
 
 results = ORC_model.predict(license_img)
-plot_bboxes(license_img, results[0].boxes.boxes, score=False, conf=0.5)
+#print(results[0].boxes.boxes)
+plot_bboxes(license_img, results[0].boxes.boxes, score=False, conf=0.4)
