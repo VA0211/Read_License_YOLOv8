11e50352ad82238faa8db7f2fe7c661141ee79c7
Your Name <you@example.com>
2023-04-06 10:02:19 +0700

    add-model-batch-1-4-23

--- test/test.py
+++ test/test.py
@@ -6,12 +6,12 @@ from return_string import print_plate
 from pre_process import pre_processing
 
 detect_model = YOLO("D:/TIPA/License/Read_License_YOLOv8/models/Detect.pt")
-ORC_model = YOLO("D:/TIPA/License/Read_License_YOLOv8/models/ORC.pt")
+ORC_model = YOLO("D:/TIPA/License/Read_License_YOLOv8/models/OCR-batch-1-4-23.pt")
 
-image_name = "IPSS2.7_s7aw.cn_2017-10-24.12-14-05.jpg"
+image_name = "IPSS2.7_s244.6_2017-12-07.14-52-26.jpg"
 #image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/{image_name}")
 #image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/TEST BIEN SO/TEST BIEN SO/{image_name}")
-image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/TEST BIEN SO/TEST BIEN SO/no_support/{image_name}")
+image = cv2.imread(f"D:/TIPA/License/Read_License_YOLOv8/test/test_img/TEST BIEN SO/no_support/{image_name}")
 image_arr = np.asarray(image)
 
 license_results = detect_model.predict(image_arr)
@@ -28,5 +28,5 @@ for box, conf, label in zip(boxes, confidences, labels):
 license_img = pre_processing(image_crop)
 
 results = ORC_model.predict(license_img)
-#plot_bboxes(license_img, results[0].boxes.boxes, score=False, conf=0.4)
-print_plate(license_img, results[0].boxes.boxes)
+plot_bboxes(license_img, results[0].boxes.boxes, score=False)
+#print_plate(license_img, results[0].boxes.boxes)
